1706d8411df1d67ea61eaec46fbfee22beee8832
saumyaranjan swain <swains313@gmail.com>
2023-03-10 19:43:47 +0530

    commited weather project

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Tempapp from './component/Tempapp';
+
+function App() {
+  return (
+   <div>
+    <Tempapp/>
+   </div>
+  );
+}
+
+export default App;
---
+++ project/src/component/Tempapp.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react'
+import './css/style.css'
+import axios from "axios"
+
+
+
+const Tempapp = () => {
+
+    const [search, setSearch] = useState("");
+    const [city, setCity] = useState("");
+
+    
+
+
+    useEffect(()=>{
+      const url=`http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=d3b99b66a3b5f50840624a2372978453` 
+      axios.get(url)
+      .then((response)=>{
+        console.log(response);
+      setCity(response.data);
+      })
+      .catch(()=>{
+        console.log("data not fetched");
+      })
+    },[search])
+
+
+  return (
+    <section className='box'>
+
+
+      <div className='box1'>
+
+        <div className='box1right'>
+
+          <div className='container'>
+            
+            <input type="search" value={search} placeholder="Enter the city name" style={{marginTop:"4px",background: "transparent", color:"white", height:"40px",width:"80%"}} className="search" onChange={(e)=>{setSearch(e.target.value)}} />
+            {!city ? (
+              <div className='container'> <p className='container'>Not Found</p></div>
+           
+            ):(
+
+            <div className='container'>
+
+              <form>
+                <table className='container' style={{marginTop: "100px"}}>
+                  <tr>
+                  <td>
+                   City : {city.name}, {city.sys.country}
+                    </td>
+                  </tr>
+                  <tr>
+                  <td>
+                    Weather : {city.weather[0].description}
+                  </td>
+                  </tr>
+                  <tr>
+                  <td>
+                Temprature : {city.main.temp} °c
+                   </td>
+                  </tr>
+                  <tr>
+                  <td>
+                    Humidity : {city.main.humidity} %
+                  </td>
+                  </tr>
+                  <tr>
+                  <td>
+                    Visibility : {city.visibility} mi
+                  </td>
+                  </tr>
+                  <tr>
+                  <td>
+                   Wind Speed : {city.wind.speed} km/h
+                   </td>
+                  </tr>
+
+                </table>
+              </form>
+
+            </div>
+            )}
+          </div>
+
+        </div>
+
+
+
+
+
+
+      </div>
+
+
+
+
+    </section>
+  )
+}
+
+export default Tempapp
